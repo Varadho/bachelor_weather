a25896f9016ba8a1d6bc607f024754f53633dfd4
Mateusz Czajkowski <mateusz.czajkowski@mni.thm.de>
2020-10-27 10:05:26 +0100

    first test implementation (currently broken for some reason)

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,32 +1,36 @@
+import 'package:bachelorweather/utility/constants/enums.dart';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Counter App', () {
-    // First, define the Finders and use them to locate widgets from the
-    // test suite. Note: the Strings provided to the `byValueKey` method must
-    // be the same as the Strings we used for the Keys in step 1.
+  group('Profiling', () {
+    FlutterDriver driver;
+
     final timeSelector = find.byValueKey("ts");
-    final locationSelector = find.byValueKey("ls");
     final incrementTimeButton = find.byValueKey('next_time');
     final decrementTimeButton = find.byValueKey('previous_time');
-
-    FlutterDriver driver;
 
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
 
-    test('starts around now', () async {
-      var now = DateTime.now();
-    });
+    test('setState', () async {
+      final homeTile = find.byValueKey(StateMethod.SS.name);
+      final timeline = await driver.traceAction(() async {
+        await driver.tap(homeTile);
+        await driver.tap(timeSelector);
+        for (var i = 0; i < 40; i++) {
+          await driver.tap(incrementTimeButton);
+        }
+      });
+      final summary = TimelineSummary.summarize(timeline);
 
-    test('increments the counter', () async {
-      // First, tap the button.
-      for (var i = 0; i < 35; i++) {
-        await driver.tap(incrementTimeButton);
-      }
+      await summary.writeSummaryToFile('scrolling_summary', pretty: true);
+      // Optionally, write the entire timeline to disk in a json format. This
+      // file can be opened in the Chrome browser's tracing tools found by
+      // navigating to chrome://tracing.
+      await summary.writeTimelineToFile('scrolling_timeline', pretty: true);
     });
 
     // Close the connection to the driver after the tests have completed.
